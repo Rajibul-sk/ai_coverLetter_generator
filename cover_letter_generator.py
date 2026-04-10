591c6608a24b8c38644fe80ac27ada164a8c07dc
Rajibul-sk <rajibulsk833@gmail.com>
2026-04-10 11:18:59 +0530

    Main code file

---
+++ cover_letter_generator.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import google.generativeai as genai
+genai.configure(api_key = st.secrets["GOOGLE_GEMINI_KEY"])
+use_model = genai.GenerativeModel("gemini-2.5-flash")
+st.title("AI-Powered Cover Letter Generator")
+job_title = st.text_input("Enter Job Title")
+resume_summary = st.text_area("Enter Resume Summary")
+if st.button("Generate Cover Letter"):
+    if job_title and resume_summary:
+        prompt = f"Write A Cover Letter for {job_title} using these resume points: {resume_summary}"
+        response = use_model.generate_content(prompt)
+        st.write(response.text)
+else:
+    st.warning("Please provide both job title and resume summary.")
